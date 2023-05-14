f9e6a6d0f7e0a99a1efe565e0e97f69b04909e1c
Yurovskikh <yurovskikh.yasmin@yandex.ru>
2023-05-14 18:53:52 +0300

    feat: proptypes

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -2,21 +2,25 @@
 import {Button, CurrencyIcon, DeleteIcon, LockIcon, DragIcon  } from "@ya.praktikum/react-developer-burger-ui-components";
 import burgerConstructorStyles from "../burger-constructor/burger-constructor.module.css";
 import React from "react";
-import burgerIngredStyles from "../burger-ingredients/burger-ingredients.module.css";
 
-const Position = ({ ingrdData, isBun = false}) => {
+const Position = ({ ingrdData, bunType = "mainBun"}) => {
+
+    const backgroundType = bunType === "mainBun" ? {borderRadius: "40px"}
+        : bunType === "upBun" ? {borderRadius: "88px 88px 40px 40px"} : {borderRadius: "40px 40px 88px 88px"}
+
     return (
-        <div className={` ${burgerConstructorStyles.position}`}>
-            {!isBun && <DragIcon type="primary" /> || <div></div>}
-            <div className={`${burgerConstructorStyles.ingredient} pr-5 pl-2 pt-4 pb-4`}>
+        <div className={`${burgerConstructorStyles.position}`}>
+            {(bunType === "mainBun") && <DragIcon type="primary" /> || <div></div>}
+
+            <div className={`${burgerConstructorStyles.ingredient} pr-5 pl-2 pt-4 pb-4`} style={backgroundType}>
                 <img className={burgerConstructorStyles.ingrdImage} src={ingrdData.image} alt="ingredient"/>
                 <p className={`text text_type_main-default`}>{ingrdData.name}</p>
                 <div className={burgerConstructorStyles.price}>
                     <p className="text text_type_digits-default pr-2">{ingrdData.price}</p>
                     <CurrencyIcon type="primary" />
                 </div>
-                {isBun && <LockIcon type="primary" />}
-                {!isBun && <DeleteIcon type="primary" />}
+                {(bunType !== "mainBun") && <LockIcon type="primary" />}
+                {(bunType === "mainBun") && <DeleteIcon type="primary" />}
             </div>
         </div>
     );
@@ -33,7 +37,7 @@ const BurgerConstructor = ({ ingredients }) => {
     return (
         <div className={`${burgerConstructorStyles.container} pt-15 pb-4`}>
             <div className="pr-4">
-                <Position ingrdData={buns[0]} isBun={true} />
+                <Position ingrdData={buns[0]} bunType="upBun" />
             </div>
 
             <div className={`${burgerConstructorStyles.mainContainer} pr-1 custom-scroll`}>
@@ -44,11 +48,11 @@ const BurgerConstructor = ({ ingredients }) => {
                     })
                 }
             </div>
-            <div className="pr-4">
-                <Position ingrdData={buns[1]} isBun={true} />
+            <div className="pr-4 pb-1">
+                <Position ingrdData={buns[1]} bunType="downBun" />
             </div>
 
-            <div className={`${burgerConstructorStyles.orderContainer} mt-4 mb-4`}>
+            <div className={`${burgerConstructorStyles.orderContainer} mt-5 mb-5`}>
                 <div className={burgerConstructorStyles.price}>
                     <p className="text text_type_digits-medium pr-2">610</p>
                     <CurrencyIcon type="primary" />
@@ -61,4 +65,5 @@ const BurgerConstructor = ({ ingredients }) => {
     );
 }
 
-export default BurgerConstructor;;
+
+export default BurgerConstructor;
--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -1,5 +1,19 @@
 import PropTypes from "prop-types";
+import BurgerConstructor from "../components/burger-constructor/burger-constructor";
+import BurgerIngredients from "../components/burger-ingredients/burger-ingredients";
 
 export const ingredientPropType = PropTypes.shape({
-  //укажите здесь prop-types для ингридиента
+    _id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired
 });
+
+BurgerConstructor.propTypes = {
+    ingredients: PropTypes.arrayOf(ingredientPropType).isRequired
+}
+
+BurgerIngredients.propTypes = {
+    ingredients: PropTypes.arrayOf(ingredientPropType).isRequired
+}

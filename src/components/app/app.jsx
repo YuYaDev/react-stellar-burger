2dac39f4394333dca8e22465bdc65a672b2f8b69
Yurovskikh <yurovskikh.yasmin@yandex.ru>
2023-07-31 16:01:23 +0300

    feat: get ingredients

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,47 +2,24 @@ import React, { useEffect } from "react";
 import styles from "./app.module.css";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import Api from "../../utils/api";
-import { IngredientContext } from "../../utils/contexts";
-import { useSelector, useDispatch } from "react-redux";
-import { getMenuIngredients } from "../../services/actions/ingredients";
 
 function App() {
-   const { ingredientsRequest, ingredientsFailed, ingredientList }  = useSelector(
-     (store) => store.ingredients
-   );
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getMenuIngredients());
-  }, []);
-
-  return (
-    <>
-      {ingredientsRequest && <p>Загрузка...</p>}
-      {ingredientsRequest === false && ingredientsFailed && (
-        <p>Произошла ошибка при получении данных</p>
-      )}
-      {ingredientsRequest === false && ingredientList.length > 0 && (
-        <div className={styles.container}>
-          <div className={styles.app}>
-            <AppHeader />
-            <main className={styles.main}>
-              <div>
-                <p className="text text_type_main-large pb-5">
-                  Соберите бургер
-                </p>
-                <BurgerIngredients />
-              </div>
-               <IngredientContext.Provider value={ingredientList}>
-                           <BurgerConstructor />
-               </IngredientContext.Provider>
-            </main>
+    return (
+    <div className={styles.container}>
+      <div className={styles.app}>
+        <AppHeader />
+        <main className={styles.main}>
+          <div>
+            <p className="text text_type_main-large pb-5">
+              Соберите бургер
+            </p>
+            <BurgerIngredients />
           </div>
-        </div>
-      )}
-    </>
+           {/*<BurgerConstructor />*/}
+        </main>
+      </div>
+    </div>
   );
 }
 
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,30 +1,41 @@
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./burger-ingredients.module.css";
-import React, { useContext, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState} from "react";
 import Ingredient from "../ingredient/ingredient";
 import Modal from "../modal/modal";
 import IngredientDetails from "../ingredient-details/ingredient-details";
-import { useSelector } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {getMenuIngredients} from "../../services/actions/ingredients";
 
 const BurgerIngredients = () => {
-  const { ingredientList: ingredients } = useSelector(
-    (store) => store.ingredients
-  );
 
+  // Get ingredients from server
+  const { ingredientsRequest, ingredientsFailed, ingredientList }  = useSelector(
+      (store) => store.ingredients
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMenuIngredients());
+  }, []);
+
+
+  // Parse ingredients
   const [current, setCurrent] = React.useState("leaf");
   const bunList = useMemo(
-    () => ingredients.filter((item) => item.type === "bun"),
-    [ingredients]
+    () => ingredientList.filter((item) => item.type === "bun"),
+    [ingredientList]
   );
   const sauceList = useMemo(
-    () => ingredients.filter((item) => item.type === "sauce"),
-    [ingredients]
+    () => ingredientList.filter((item) => item.type === "sauce"),
+    [ingredientList]
   );
   const mainList = useMemo(
-    () => ingredients.filter((item) => item.type === "main"),
-    [ingredients]
+    () => ingredientList.filter((item) => item.type === "main"),
+    [ingredientList]
   );
 
+  // Modals
   const [isModalVisible, setModalVisible] = useState(false);
   const [modalData, setModalData] = useState({});
 
@@ -45,69 +56,74 @@ const BurgerIngredients = () => {
   return (
     <>
       {isModalVisible && modal}
+      {ingredientsRequest && <p>Загрузка...</p>}
+      {ingredientsRequest === false && ingredientsFailed && (
+          <p>Произошла ошибка при получении данных</p>
+      )}
+      {ingredientsRequest === false && ingredientList.length > 0 && (
+          <>
+            <div className={`${styles.tabWrapper} mb-5`}>
+            <Tab value="leaf" active={current === "leaf"} onClick={setCurrent}>
+              Булки
+            </Tab>
+            <Tab value="sauce" active={current === "sauce"} onClick={setCurrent}>
+              Соусы
+            </Tab>
+            <Tab
+              value="filling"
+              active={current === "filling"}
+              onClick={setCurrent}
+            >
+              Начинки
+            </Tab>
+          </div>
 
-      <div className={`${styles.tabWrapper} mb-5`}>
-        <Tab value="leaf" active={current === "leaf"} onClick={setCurrent}>
-          Булки
-        </Tab>
-        <Tab value="sauce" active={current === "sauce"} onClick={setCurrent}>
-          Соусы
-        </Tab>
-        <Tab
-          value="filling"
-          active={current === "filling"}
-          onClick={setCurrent}
-        >
-          Начинки
-        </Tab>
-      </div>
+          <div className={`${styles.container} custom-scroll`}>
+            <p className="text text_type_main-medium pt-5 pb-2" id="leaf">
+              Булки
+            </p>
+            <div className={`${styles.gridWrapper} mb-5 ml-4 mr-4 mt-4`}>
+              {bunList.map((item, index) => {
+                return (
+                  <Ingredient
+                    ingredientData={item}
+                    key={index}
+                    openModal={handleModalOpen}
+                  />
+                );
+              })}
+            </div>
 
-      <div className={`${styles.container} custom-scroll`}>
-        <p className="text text_type_main-medium pt-5 pb-2" id="leaf">
-          Булки
-        </p>
-        <div className={`${styles.gridWrapper} mb-5 ml-4 mr-4 mt-4`}>
-          {bunList.map((item, index) => {
-            return (
-              <Ingredient
-                ingredientData={item}
-                key={index}
-                openModal={handleModalOpen}
-              />
-            );
-          })}
-        </div>
+            <p className="text text_type_main-medium pt-5 pb-2" id="sauce">
+              Соусы
+            </p>
+            <div className={`${styles.gridWrapper} mb-5 ml-4 mr-4 mt-4`}>
+              {sauceList.map((item, index) => {
+                return (
+                  <Ingredient
+                    ingredientData={item}
+                    key={index}
+                    openModal={handleModalOpen}
+                  />
+                );
+              })}
+            </div>
 
-        <p className="text text_type_main-medium pt-5 pb-2" id="sauce">
-          Соусы
-        </p>
-        <div className={`${styles.gridWrapper} mb-5 ml-4 mr-4 mt-4`}>
-          {sauceList.map((item, index) => {
-            return (
-              <Ingredient
-                ingredientData={item}
-                key={index}
-                openModal={handleModalOpen}
-              />
-            );
-          })}
-        </div>
-
-        <p className="text text_type_main-medium pt-5 pb-2" id="filling">
-          Начинки
-        </p>
-        <div className={`${styles.gridWrapper} mb-5 ml-4 mr-4 mt-4`}>
-          {mainList.map((item, index) => {
-            return (
-              <Ingredient
-                ingredientData={item}
-                key={index}
-                openModal={handleModalOpen}
-              />
-            );
-          })}
-        </div>
-      </div>
+            <p className="text text_type_main-medium pt-5 pb-2" id="filling">
+              Начинки
+            </p>
+            <div className={`${styles.gridWrapper} mb-5 ml-4 mr-4 mt-4`}>
+              {mainList.map((item, index) => {
+                return (
+                  <Ingredient
+                    ingredientData={item}
+                    key={index}
+                    openModal={handleModalOpen}
+                  />
+                );
+              })}
+            </div>
+        </div></>)}
     </>
   );
 };
